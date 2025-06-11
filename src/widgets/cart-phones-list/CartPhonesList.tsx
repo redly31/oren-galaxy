b505e21a483b1cc4473d9fab82dfeb577c9e745f
redly31 <redlyyyyyyyyyyy@gmail.com>
2025-06-11 18:15:24 +0500

    refactor: cart phones list

---
+++ src/widgets/cart-phones-list/CartPhonesList.tsx
@@ -0,0 +1,26 @@
+import CartPhoneCard from "../../entities/cart/CartPhoneCard";
+import { useCartPhones } from "../../entities/cart/useCartPhones";
+import PaymentButton from "../../features/payment-button/PaymentButton";
+import RemoveFromCartButton from "../../features/remove-from-cart-button/RemoveFromCartButton";
+import ErrorSign from "../../shared/components/error/ErrorSign";
+import Loading from "../../shared/components/loading/Loading";
+
+export default function CartPhonesList() {
+
+    const { items, isLoading, isError } = useCartPhones();
+    
+      if (isLoading) return <Loading />;
+      if (isError || !Array.isArray(items)) return <ErrorSign />;
+
+  return (
+    <div className="grid justify-start grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+      {items.map((phone) => (
+        <CartPhoneCard data={phone} key={phone.id}>
+          <PaymentButton productId={phone.id} />
+          <RemoveFromCartButton productId={phone.id} />
+        </CartPhoneCard>
+      ))}
+      {items.length === 0 && <h1 className="flex mt-8">Здесь ничего нет😢</h1>}
+    </div>
+  );
+}
